0c195faf37e43680621059c96a672dcdc9d9d9bf
gohil-db <gohildb2025@gmail.com>
2025-07-18 11:36:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BarChartComponent from "./components/SimpleChart";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center mb-4">Dashboard</h2>
+      <BarChartComponent />
+    </div>
+  );
+}
+
+export default App;
